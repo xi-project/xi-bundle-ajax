d47a831f6f733d7a7e54c970a5c5bbf21d313849
Mikko Hirvonen <mikko.petteri.hirvonen@gmail.com>
2012-05-29 17:24:05 +0300

    Fixed controller test.

--- Tests/Model/TestUser.php
+++ Tests/Model/TestUser.php
@@ -2,15 +2,12 @@
 
 namespace Xi\Bundle\AjaxBundle\Tests\Model;
 
-use Symfony\Component\Validator\Constraints as Assert;
-
 /**
  * @author Mikko Hirvonen <mikko.petteri.hirvonen@gmail.com>
  */
 class TestUser
 {
     /**
-     * @Assert\NotBlank()
      * @var string
      */
     public $name;
